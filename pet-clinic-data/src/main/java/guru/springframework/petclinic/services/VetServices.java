bfa6f78145342eca8324a305c54aadf5bc751fd9
Rick <tillett.eric@gmail.com>
2020-05-21 16:23:42 -0400

    Created CRUD service for other services to extend.  Closes #12.

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/services/VetServices.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/VetServices.java
@@ -2,13 +2,6 @@ package guru.springframework.petclinic.services;
 
 import guru.springframework.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetServices extends CrudService<Vet, Long> {
 
-public interface VetServices {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
