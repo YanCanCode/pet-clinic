91bee78709a8f02dc58221a50e93df80517b0252
Rick <tillett.eric@gmail.com>
2020-05-25 18:36:55 -0400

    Added PetType to bootstrap startup (DataInitializer) . Closes #33.

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/services/PetTypeService.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/PetTypeService.java
@@ -1,7 +1,6 @@
 package guru.springframework.petclinic.services;
 
-import guru.springframework.petclinic.model.BaseEntity;
 import guru.springframework.petclinic.model.PetType;
 
-public interface PetTypeService<P extends BaseEntity, L extends Number> extends CrudService<PetType, Long> {
+public interface PetTypeService extends CrudService<PetType, Long> {
 }
--- pet-clinic-web/src/main/java/guru/springframework/petclinic/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/guru/springframework/petclinic/bootstrap/DataInitializer.java
@@ -1,8 +1,10 @@
 package guru.springframework.petclinic.bootstrap;
 
 import guru.springframework.petclinic.model.Owner;
+import guru.springframework.petclinic.model.PetType;
 import guru.springframework.petclinic.model.Vet;
 import guru.springframework.petclinic.services.OwnerService;
+import guru.springframework.petclinic.services.PetTypeService;
 import guru.springframework.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,17 +13,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class DataInitializer implements CommandLineRunner {
 
- private final OwnerService ownerService;
+    private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataInitializer(OwnerService ownerService, VetService vetService) {
+    public DataInitializer(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("dog");
+        PetType savedPetDog = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Larry");
