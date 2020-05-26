d85a53d395d0c8c7a621dfd7a2560084ee992f3e
Rick <tillett.eric@gmail.com>
2020-05-26 12:03:10 -0400

    Implemented specialty service and service map. Closes #32.

---
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/SpecialtyService.java
@@ -0,0 +1,7 @@
+package guru.springframework.petclinic.services;
+
+import guru.springframework.petclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+
+}
